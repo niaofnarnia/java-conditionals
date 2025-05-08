e120906174f4f7442342ba65369924e4ec206bcc
Nia <niaespinal@outlook.com>
2025-05-08 14:36:41 +0200

    Completed Quiz exercise

--- Quiz.java
+++ Quiz.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Quiz {
     public static void main(String[] args) {
         //Escribe un programa que sea un pequeño juego trivial de acertar preguntas
@@ -36,31 +37,61 @@ public class Quiz {
         */
 
         //Respuestas: 1. b / 2. c / 3. b y d / 4. a
-        System.out.println("1. ¿Quién fue la hacker que escribió en 1994 Hacking the Wetware?");
+    Scanner scanner = new Scanner(System.in);
+            int points = 0;
+        System.out.print("1. ¿Quién fue la hacker que escribió en 1994 Hacking the Wetware?\n");
             String choices1 = "a. Ada Lovelace\n" +
                     "b. Jude Milhon\n" +
                     "c. Martha Ackelsberg\n" +
                     "d. Jane Jacobs";
-                System.out.println(choices1.indent(3));
-        System.out.println("\n2. ¿Quién es Fuencisla Clemares?");
+                System.out.print(choices1.indent(3));
+                System.out.println("Responde: ");
+                    String answer1 = scanner.nextLine();
+                        if (answer1.equals("b")) {
+                            points += 5;
+                        }
+        System.out.print("\n2. ¿Quién es Fuencisla Clemares?\n");
             String choices2 = "a. La directora general de IBM Europa, Oriente Medio y África\n" +
                     "b. La vicepresidenta de Microsoft Western Europe\n" +
                     "c. La directora general de Google en España y Portugal\n" +
                     "d. Fundadora y CEO de Marsi Bionics";
-                System.out.println(choices2.indent(3));
-        System.out.println("\n3. ¿Quién fue Hedwig Eva Maria Kiesler, conocida como Hedy Lamarr?");
+                System.out.print(choices2.indent(3));
+                System.out.println("Responde: ");
+                    String answer2 = scanner.nextLine();
+                        if (answer2.equals("c")) {
+                            points += 5;
+                        }
+        System.out.print("\n3. ¿Quién fue Hedwig Eva Maria Kiesler, conocida como Hedy Lamarr?\n");
             String choices3 = "a. Primera programadora de software\n" +
                     "b. Una actriz de cine\n" +
                     "c. Inventora del procesador de datos\n" +
                     "d. Inventora del sistema de comunicaciones de salto de frecuencia, base del wifi, bluetooth y GPS";
-                System.out.println(choices3.indent(3));
-        System.out.println("\n4. ¿Cómo se llamaban las principales 'mujeres ocultas' de la NASA, encargadas de los cálculos para que la misión del Apolo 11 concluyera con éxito?");
+                System.out.print(choices3.indent(3));
+                System.out.println("Responde: ");
+                    String answer3 = scanner.nextLine();
+                        if(answer3.contains("b") && answer3.contains("d") && answer3.length() <=3) {
+                            points += 5;
+                        }
+        System.out.print("\n4. ¿Cómo se llamaban las principales 'mujeres ocultas' de la NASA, encargadas de los cálculos para que la misión del Apolo 11 concluyera con éxito?\n");
             String choices4 = "a. Katherine Johnson, Dorothy Vaughan y Mary Jackson\n" +
                     "b. Jessica Miller, Emily Johnson, Sarah Davis\n" +
                     "c. bell hooks, Toni Morrison y Alice Walker\n" +
                     "d. Emmeline Pankhurst, Millicent Fawcett y Harriet Harman";
-                System.out.println(choices4.indent(3));
-
+                System.out.print(choices4.indent(3));
+                System.out.println("Responde: ");
+                    String answer4 = scanner.nextLine();
+                        if (answer4.equals("a")) {
+                            points += 5;
+                        }
+        System.out.println("Tu puntaje fue: " + points);
+            if (points >= 15) {
+                System.out.println("Tú sí que sabes mucho.");
+            } else if (points > 15 && points >= 5) {
+                System.out.println("No lo has hecho mal.");
+            } else {
+                System.out.println("Buena suerte la próxima vez.");
+            }
+        scanner.close();
     }
     
 }
